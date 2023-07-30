36b25e2bd6fd841e79141d728f6245d9989f7a9a
libyzxy0 <janlibydelacosta@gmail.com>
2023-07-30 06:52:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const tiktok = require("./tiktok-url-handler");
+const express = require("express");
+const app = express();
+
+app.get('/', async (req, res) => {
+  if(!!req.query.url) {
+    let data = await tiktok.getVideoInfo(req.query.url);
+    res.type('json').send(JSON.stringify(data, null, 2) + '\n');
+  } else {
+    res.type('json').send(JSON.stringify({ message: "Wheres the url" }, null, 2) + '\n');
+  }
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App is listening on port ${port}`))
